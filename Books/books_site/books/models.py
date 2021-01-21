d0d5239730e914e7edb36332c3f6d645d68e2e46
Giorgi1971 <giooorgi7@gmail.com>
2021-01-22 00:40:09 +0400

    created authors and books in database, also admin page

--- Books/books_site/books/models.py
+++ Books/books_site/books/models.py
@@ -3,28 +3,6 @@ from django.db import models
 from books.choices import GenreChoices, CondChoices, StatusChoices
 
 
-class BookModel(models.Model):
-    title = models.CharField(max_length=100, verbose_name="Book Name")
-    created = models.DateTimeField(verbose_name="Added Date/Time", auto_now_add=True)
-    updated = models.DateTimeField(verbose_name="Updated Date/Time", auto_now=True)
-    quantity = models.PositiveSmallIntegerField(verbose_name="Stock", default=1)
-    author = models.ForeignKey(to='store.AuthorModel', on_delete=models.PROTECT)
-    genre = models.PositiveSmallIntegerField("Gender", choices=GenreChoices.choices, default=GenreChoices.Other)
-    condition = models.PositiveSmallIntegerField("Gender", choices=CondChoices.choices, default=CondChoices.Other)
-    status = models.PositiveSmallIntegerField("Gender", choices=StatusChoices.choices, default=StatusChoices.Other)
-
-    class Meta:
-        # unique_together = ('name', 'author')
-        verbose_name = 'Book'
-        verbose_name_plural = 'Books'
-
-    def __str__(self):
-        return self.title
-
-    def get_quantity(self):
-        return f'რაოდენობაშია: {self.quantity}'
-
-
 class AuthorModel(models.Model):
     full_name = models.CharField("Full Name", max_length=255)
     age = models.PositiveSmallIntegerField("Age")
@@ -38,3 +16,25 @@ class AuthorModel(models.Model):
 
     def get_info(self):
         return f'Age: {self.age}'
+
+
+class BookModel(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Book Name")
+    created = models.DateTimeField(verbose_name="Added Date/Time", auto_now_add=True)
+    updated = models.DateTimeField(verbose_name="Updated Date/Time", auto_now=True)
+    quantity = models.PositiveSmallIntegerField(verbose_name="Stock", default=1)
+    author = models.ForeignKey(to='books.AuthorModel', on_delete=models.PROTECT)
+    genre = models.PositiveSmallIntegerField("Gender", choices=GenreChoices.choices, default=GenreChoices.Fiction)
+    condition = models.PositiveSmallIntegerField("Gender", choices=CondChoices.choices, default=CondChoices.New)
+    status = models.PositiveSmallIntegerField("Gender", choices=StatusChoices.choices, default=StatusChoices.Sale)
+
+    class Meta:
+        # unique_together = ('name', 'author')
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+
+    def __str__(self):
+        return self.title
+
+    def get_quantity(self):
+        return f'რაოდენობაშია: {self.quantity}'
